64725753e8e1539e3bf332590ed89c3a3f813010
omnia197 <omnia18ayman@gmail.com>
2026-01-30 19:28:16 +0200

    exp: add the experiment on foveated classification on cifar100

---
+++ foveated_exp/seed.py
@@ -0,0 +1,12 @@
+
+import torch
+import numpy as np
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    import random
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
